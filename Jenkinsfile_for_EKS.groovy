1b4df05b3c3f45dbc01f804026ee4f7b4279d575
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-02-08 12:45:08 -0600

    added jenkinsfile for eks

---
+++ Jenkinsfile_for_EKS.groovy
@@ -0,0 +1,16 @@
+node {
+    stage ("Pull Repo"){
+        git "https://github.com/bkarakas/terraform-iaac-eks-burak.git"
+     
+    }
+    stage ("stage1"){
+        echo "Hello"
+
+    }
+    stage ("stage1"){
+        echo "Hello"
+    }
+    stage ("stage1"){
+        echo "Hello"
+    }
+}
