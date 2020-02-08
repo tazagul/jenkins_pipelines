1362e875d37a5950db1d56dad5a7af4e0b6001f5
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-02-08 10:32:38 -0600

    packer

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,14 @@
-
+node {
+    stage("Pull Repo"){
+        echo "Hello"
+    }
+     stage("Build Image"){
+        sh "packer version"
+    }
+     stage("Send Notificatons to Slack"){
+        echo "Hello"
+    }
+     stage("Send Email"){
+        echo "Hello"
+    }
+}
