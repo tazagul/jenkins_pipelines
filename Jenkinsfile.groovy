16aa7a589758ac97cca5d0d375d73a64afd60652
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-02-08 12:23:21 -0600

    assd

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,33 +1,38 @@
 node {
-   properties([parameters([
-       choice(choices: [
-           'golden_ami',
-           'tower', 
-           'elk', 
-           'nagios', 
-           'gitlab', 
-           'nexus', 
-           'vault'
-           ], description: 'what tool would you like to build? ', name: 'TOOL_TO_PROVISION'), 
-       text(defaultValue: 'dummy@gmail.com', description: 'Please provide email(s) for notifications. Use , for multiple emails', name: 'EMAIL_TO_SEND')
-       choice(choices: [
-           'us-east-1', 
-           'us-east-2', 
-           'us-west-1', 
-           'us-west-2'
-           ], description: 'please choose a region', name: 'AMI_REGION')])])
+    properties([parameters([
+        choice(choices: [
+            'golden_ami', 
+            'tower', 
+            'elk', 
+            'nagiosxi', 
+            'gitlab', 
+            'nexus', 
+            'vault',
+            'artemisv1'
+            ], 
+            description: 'What tool would you like to build?', name: 'TOOL_TO_PROVISION'),
+        text(defaultValue: 'dummy@gmail.com', description: 'Please provide email(s) for notifications. Use , for multiple emails', name: 'EMAIL_TO_SEND'),
+        choice(choices: [
+            'us-east-1', 
+            'us-east-2', 
+            'us-west-1', 
+            'us-west-2',
+            'eu-west-1',
+            'eu-west-2'
+            ], 
+            description: 'Please choose a region', name: 'AMI_REGION')])])
+
     stage("Pull Repo"){
         git 'https://github.com/farrukh90/packer.git'
     }
-     stage("Build Image"){
+    stage("Build Image"){
         sh "packer version"
-        //sh "packer build -var region=${AMI_REGION} tools/${TOOL_TO_PRIVISION}.json"
-        
+        //sh "packer  build -var region=${AMI_REGION} tools/${TOOL_TO_PROVISION}.json"
     }
-     stage("Send Notificatons to Slack"){
+    stage("Send Notification to Slack"){
         slackSend channel: 'nagios_alerts', message: "${TOOL_TO_PROVISION} has been built"
     }
-     stage("Send Email"){
+    stage("Send Email"){
         mail bcc: '', 
         body: "Hello, Your AMI is ready in ${AMI_REGION} Thanks", 
         cc: '', 
