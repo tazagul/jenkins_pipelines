91ba1357c026fb5d3bb8d48a8f4bcf85aee1f665
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-02-10 18:18:29 -0600

    artemis

---
+++ Jenkins_for_Artemis.groovy
@@ -0,0 +1,75 @@
+node {
+	properties(
+		[parameters(
+		[choice(choices:
+		[
+		'version/0.1',
+		'version/0.2',
+		'version/0.3',
+		'version/0.4',
+		'version/0.5',
+		'version/0.6',
+		'version/0.7',
+		'version/0.8',
+		'version/0.9',
+		'version/0.10'],
+	description: 'Which version of the app should I deploy? ',
+	name: 'Version'),
+	choice(choices:
+	[
+		'dev1.tazagul.net',
+		'qa1.tazagul.net',
+		'stage1.tazagul.net',
+		'prod1.tazagul.net'],
+	description: 'Please provide an environment to build the application',
+	name: 'ENVIR'),
+	string(defaultValue: 'kumarova.t@gmail.com', description: 'please enter an email to receive Artemis deploy', name: 'EMAIL_TO_SEND', trim: false)
+	])])
+	stage("Stage1"){
+		timestamps {
+			ws {
+				checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/fuchicorp/artemis.git']]])
+		}
+	}
+}
+	stage("Install Prerequisites"){
+		timestamps {
+			ws{
+				sh '''
+					ssh centos@${ENVIR} sudo yum install epel-release -y
+					ssh centos@${ENVIR} sudo yum install python-pip -y
+					ssh centos@${ENVIR} sudo pip install Flask
+					'''
+		}
+	}
+}
+	stage("Copy Artemis"){
+		timestamps {
+			ws {
+				sh '''
+					scp -r * centos@${ENVIR}:/tmp
+					'''
+		}
+	}
+}
+	stage("Run Artemis"){
+		timestamps {
+			ws {
+				sh '''
+					ssh centos@${ENVIR} nohup python /tmp/artemis.py  &
+					'''
+		}
+	}
+}
+	stage("Send slack notifications"){
+		timestamps {
+			ws {
+				echo "Slack"
+				//slackSend color: '#BADA55', message: 'Hello, World!'
+			}
+		}
+		
+	stage ("send email")
+	mail bcc: '', body: "Hello! Artemis ${Version} deployed to ${Envir}" , cc: '', from: '', replyTo: '', subject: "Artemis version deployed to environment", to: "${EMAIL_TO_SEND}"
+	}
+}
